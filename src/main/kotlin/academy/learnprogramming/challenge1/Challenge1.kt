6ef7e39b45cc02e3c07d28ade0ffd43f30e3ae4a
Emad Zangeneh <emad.zangeneh@gmail.com>
2021-04-11 22:27:11 +0430

    Second Commit
    This is so bad

--- src/main/kotlin/academy/learnprogramming/challenge1/Challenge1.kt
+++ src/main/kotlin/academy/learnprogramming/challenge1/Challenge1.kt
@@ -2,5 +2,5 @@
 
 
 fun main(args: Array<String>) {
-
+    println("This is something weird")
 }
---
+++ src/test/kotlin/academy/learnprogramming/challenge1/EmployeeTest.kt
@@ -0,0 +1,24 @@
+package academy.learnprogramming.challenge1
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class EmployeeTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun getName() {
+        assertEquals(2,2)
+    }
+}
